dd0e84308ab729d8410f1798cc0f311b9e418894
nonis3 <nonis@purdue.edu>
2017-08-07 08:17:09 -0700

    Basic run command function, spinning up a container then killing it

--- lib/docker_test.ex
+++ lib/docker_test.ex
@@ -1,2 +1,30 @@
 defmodule DockerTest do
+  def imagename, do: "nonis/baseimage"
+  
+  def run(command) do
+    result = Dockerex.Client.post("containers/create", 
+      %{"Image": imagename,
+        "Tty": true,
+        "HostConfig": %{"RestartPolicy": %{ "Name": "always"}}})
+    cid = result["Id"]
+
+    Dockerex.Client.post("containers/#{cid}/start")
+    
+    exec = Dockerex.Client.post("containers/#{cid}/exec", 
+      %{"AttachStdin": true,
+        "AttachStdout": true,
+        "AttachStderr": true,
+        "Cmd": command,
+        "DetachKeys": "ctrl-p,ctrl-q",
+        "Privileged": true,
+        "Tty": true,
+        "User": "123:456"})
+    eid = exec["Id"]
+
+    output = Dockerex.Client.post("exec/#{eid}/start", %{"Detach": false,"Tty": true})
+    IO.inspect output
+
+    Dockerex.Client.post("containers/#{cid}/kill")
+
+  end
 end
