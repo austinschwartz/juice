65ded4a81a5e3bf9d15727e7494ad06f531e59ee
nonis3 <nonis@purdue.edu>
2017-08-08 09:11:35 -0700

    Including time in response now

--- lib/docker_test.ex
+++ lib/docker_test.ex
@@ -30,7 +30,7 @@ defmodule DockerTest do
   defmodule Exec do
     def create(cid, command) do
       exec = Dockerex.Client.post("containers/#{cid}/exec", 
-        %{"AttachStdin": true,
+        %{#"AttachStdin": true,
           "AttachStdout": true,
           "AttachStderr": true,
           "Cmd": command,
@@ -44,6 +44,37 @@ defmodule DockerTest do
     def start(eid) do
       Dockerex.Client.post("exec/#{eid}/start", %{"Detach": false,"Tty": true})
     end
+  end
+
+  def build_command(user_id, problem_id, test_id, language) do
+    ["sh", "-c", 
+      case language do
+        "Java" ->
+          entrypoint = "Main"
+          infile = "/testcases/#{problem_id}/i#{test_id}.txt"
+          "java -cp #{Const.testcases_dst}/#{problem_id}/#{user_id} #{entrypoint} < #{infile}"
+        default ->
+          raise "only java for now"
+      end
+    ]
+  end
+
+  def test(user_id, problem_id, test_id, program) do
+    cid = Container.create()
+       |> Container.start()
+
+    time1 = :os.system_time(:millisecond)
+    output = Exec.create(cid, build_command(user_id, problem_id, test_id, "Java"))
+          |> Exec.start()
+    time2 = :os.system_time(:millisecond)
+
+    cid |> Container.kill()
+
+    %{
+      output: output,
+      time: time2 - time1
+    }
+
   end
 
   def run(command) do
