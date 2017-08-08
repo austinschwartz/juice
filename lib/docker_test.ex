8a553e7aed88a891069dc9c2c50607145b328dff
nonis3 <nonis@purdue.edu>
2017-08-08 08:33:30 -0700

    Abstracting some stuff out, able to compile and run files now, using a volume to share files

--- lib/docker_test.ex
+++ lib/docker_test.ex
@@ -1,30 +1,60 @@
 defmodule DockerTest do
-  def imagename, do: "nonis/baseimage"
-  
+  require MyConstants
+  alias MyConstants, as: Const
+  defmodule Container do
+    def create() do
+      result = Dockerex.Client.post("containers/create", 
+        %{"Image": Const.imagename,
+          "Tty": true,
+          "HostConfig": %{
+            "RestartPolicy": %{ "Name": "always"},
+            "Binds": [
+              Const.testcases_src <> ":" <> Const.testcases_dst <> ":ro"
+            ]
+          }
+        })
+      result["Id"]
+    end
+
+    def start(cid) do
+      Dockerex.Client.post("containers/#{cid}/start")
+      cid
+    end
+
+    def kill(cid) do
+      Dockerex.Client.post("containers/#{cid}/kill")
+      cid
+    end
+  end
+
+  defmodule Exec do
+    def create(cid, command) do
+      exec = Dockerex.Client.post("containers/#{cid}/exec", 
+        %{"AttachStdin": true,
+          "AttachStdout": true,
+          "AttachStderr": true,
+          "Cmd": command,
+          "DetachKeys": "ctrl-p,ctrl-q",
+          "Privileged": true,
+          "Tty": true,
+          "User": "123:456"})
+      exec["Id"]
+    end
+
+    def start(eid) do
+      Dockerex.Client.post("exec/#{eid}/start", %{"Detach": false,"Tty": true})
+    end
+  end
+
   def run(command) do
-    result = Dockerex.Client.post("containers/create", 
-      %{"Image": imagename,
-        "Tty": true,
-        "HostConfig": %{"RestartPolicy": %{ "Name": "always"}}})
-    cid = result["Id"]
-
-    Dockerex.Client.post("containers/#{cid}/start")
+    cid = Container.create()
+       |> Container.start()
     
-    exec = Dockerex.Client.post("containers/#{cid}/exec", 
-      %{"AttachStdin": true,
-        "AttachStdout": true,
-        "AttachStderr": true,
-        "Cmd": command,
-        "DetachKeys": "ctrl-p,ctrl-q",
-        "Privileged": true,
-        "Tty": true,
-        "User": "123:456"})
-    eid = exec["Id"]
+    output = Exec.create(cid, command)
+          |> Exec.start()
 
-    output = Dockerex.Client.post("exec/#{eid}/start", %{"Detach": false,"Tty": true})
-    IO.inspect output
+    cid |> Container.kill()
 
-    Dockerex.Client.post("containers/#{cid}/kill")
-
+    output
   end
 end
---
+++ lib/my_constants.ex
@@ -0,0 +1,6 @@
+defmodule MyConstants do
+  use Constants
+  define imagename, "nonis/baseimage"
+  define testcases_src, "/home/nonis/testcases"
+  define testcases_dst, "/testcases"
+end
