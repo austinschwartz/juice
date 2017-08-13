106116a819680bc9f53c85e13b3d317ac83d13d2
nonis3 <nonis@purdue.edu>
2017-08-13 10:42:14 -0400

    Renaming app in mix.exs

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
-defmodule DockerTest.Mixfile do
+defmodule Juice.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :docker_test,
+    [app: :juice,
      version: "0.0.1",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
